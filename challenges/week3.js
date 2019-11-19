15cecb9feb83ad11e94f11d99779e809c77b61df
iggy-pigy <ilgakokorisa@gmail.com>
2019-11-19 12:50:24 +0000

    Wrote function checkIngredients

--- challenges/week3.js
+++ challenges/week3.js
@@ -19,13 +19,44 @@ function getTotalSubjects(people) {
 return countSubjects.length;
 }
 
-
-
+const menu = [
+  {
+    name: "tofu fritters",
+    ingredients: ["tofu", "egg yolk", "breadbrumbs", "paprika"]
+  },
+  {
+    name: "black bean curry",
+    ingredients: ["black beans", "garam masala", "rice"]
+  },
+  {
+    name: "chocolate tiffin",
+    ingredients: [
+      "dark chocolate",
+      "egg",
+      "flour",
+      "brown sugar",
+      "vanilla essence"
+    ]
+  },
+  {
+    name: "hummus",
+    ingredients: ["chickpeas", "tahini", "lemon", "garlic", "salt"]
+  }
+];
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
+  for (let m of menu) { 
+    for (let i of m.ingredients) {
+    if (i === ingredient) {
+      return true;
+    }
+    }
+  }
+return false;
 }
+var result=checkIngredients(menu, "lemon");
+console.log(result);
 
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
