6065ef9407b153056275d85b8293f146a3e717c6
iggy-pigy <ilgakokorisa@gmail.com>
2019-11-19 12:51:33 +0000

    Cleaned up function checkIngredients

--- challenges/week3.js
+++ challenges/week3.js
@@ -19,30 +19,7 @@ function getTotalSubjects(people) {
 return countSubjects.length;
 }
 
-const menu = [
-  {
-    name: "tofu fritters",
-    ingredients: ["tofu", "egg yolk", "breadbrumbs", "paprika"]
-  },
-  {
-    name: "black bean curry",
-    ingredients: ["black beans", "garam masala", "rice"]
-  },
-  {
-    name: "chocolate tiffin",
-    ingredients: [
-      "dark chocolate",
-      "egg",
-      "flour",
-      "brown sugar",
-      "vanilla essence"
-    ]
-  },
-  {
-    name: "hummus",
-    ingredients: ["chickpeas", "tahini", "lemon", "garlic", "salt"]
-  }
-];
+
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
@@ -55,8 +32,7 @@ function checkIngredients(menu, ingredient) {
   }
 return false;
 }
-var result=checkIngredients(menu, "lemon");
-console.log(result);
+
 
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
